759ebe12d25418a789575d6e781f44e6db352720
WalleyemanZ <jacobzea96@gmail.com>
2023-03-19 11:52:52 -0600

    added functions to create a new role & employee, as well as update and employees role

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ db.connect(() => {
     init();
 });
 
+// function that will display the list to choose from 
 function init() {
     inquirer.prompt([
         {
@@ -43,6 +44,7 @@ function init() {
     })
 }
 
+// function to view all departments
 function viewDept() {
     db.query('SELECT * FROM departments', (err, data) => {
         console.table(data);
@@ -50,6 +52,7 @@ function viewDept() {
     })
 }
 
+// View all roles
 function viewRoles() {
     db.query('SELECT * FROM roles', (err, data) => {
         console.table(data);
@@ -57,6 +60,7 @@ function viewRoles() {
     })
 }
 
+// View all employees
 function viewEmpl() {
     db.query('SELECT * FROM employees', (err, data) => {
         console.table(data)
@@ -64,6 +68,7 @@ function viewEmpl() {
     })
 }
 
+// adds a new department to the list of departments
 function addDept() {
     inquirer.prompt([
         {
@@ -73,10 +78,90 @@ function addDept() {
         }
     ])
     .then((response) => {
-        db.query(`INSERT INTO departments (dept_name) VALUES(?)`, response.newDepartment, err => {
+        db.query(`INSERT INTO departments (dept_name) VALUES (?)`, response.newDepartment, err => {
             console.log('Department created');
             viewDept();
         })
     })
 }
 
+// add a new role to the list of roles
+function addRoles() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newRole',
+            message: 'What is the new role would you like to add?'
+        },
+        {
+            type: 'input',
+            name: 'newSalary',
+            message: 'What is the salary of this role?'
+        },
+        {
+            type: 'input',
+            name: 'deptId',
+            message: 'What is the department ID for this role?'
+        }
+    ])
+    .then ((response) => {
+        db.query(`INSERT INTO roles (title, salary, dept_id) VALUES (?, ?, ?)`, [response.newRole, response.newSalary, response.deptId], err => {
+            console.log('New role created');
+            viewRoles();
+        })
+    })
+}
+
+// adds a new employee to the list of employees
+function addEmpl() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the First Name of the new employee?'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the Last Name of the new employee?'
+        },
+        {
+            type :'input',
+            name: 'roleId',
+            message: 'What is the role ID of the new employee?'
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'What is the manager ID of this employee'
+        }
+    ])
+    .then((response) => {
+        db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [response.firstName, response.lastName, response.roleId, response.manager], err => {
+            console.log('New employee added');
+            viewEmpl();
+        })
+    })
+}
+
+// updates and employees role
+function addUpdt() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'pickedEmpl',
+            message: 'What is the ID of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'newRole',
+            message: "What is this employee's new role ID?"
+        }
+    ])
+    .then((response) => {
+        db.query(`UPDATE employees SET employees.id = ? WHERE employee.id = ?`, [response.pickedEmpl, response.newRole], err => {
+            console.log('Employee updated');
+            viewEmpl();
+        })
+    })
+}
